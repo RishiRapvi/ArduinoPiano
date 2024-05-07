45c93a54bc20f532f4361b5e300f65b2024e8bae
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2024-05-07 10:24:06 -0500

    Create ArduinoPiano.ino

---
+++ ArduinoPiano.ino
@@ -0,0 +1,26 @@
+#include "pitches.h"
+#define melodyPin 3
+
+int Do = 4;
+int Re = 5;
+int Mi = 6;
+int Fa = 7;
+int Sol = 8;
+int La = 9;
+int Ti = 10;
+int Doo = 11;
+
+const int delay = 120;
+int speaker = 3;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
