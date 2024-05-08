c4a3b50b27fee4dfe9693a86ddd26f671a2c925a
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2024-05-08 10:25:47 -0500

    replaced mario theme for lucid dreams melody

--- ArduinoPiano.ino
+++ ArduinoPiano.ino
@@ -1,26 +1,80 @@
-#include "pitches.h"
+#include "pitches.h" //this library helps us use the musical notes and initalize them
 #define melodyPin 3
+#define Delay 120
+#define Speaker melodyPin
 
-int Do = 4;
-int Re = 5;
-int Mi = 6;
-int Fa = 7;
-int Sol = 8;
-int La = 9;
-int Ti = 10;
-int Doo = 11;
+//Synopisis: This code is intended to play the simple piano notes, as well as play a special Mario theme if the last button is pressed
 
-const int delay = 120;
-int speaker = 3;
+int melodyArray[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4_ NOTE_C5}; //do, re, mi, fa, sol, la, ti, mario theme
 
+
+int melody[] = {
+  NOTE_F5, NOTE_D5, NOTE_B4, NOTE_G4,
+  NOTE_D4, NOTE_E4, NOTE_B4,
+  NOTE_D5, NOTE_B4, NOTE_G4, NOTE_D4,
+  NOTE_E4, NOTE_B4, 0,
+  NOTE_E4, NOTE_B4, 0,
+  NOTE_F5, NOTE_D5, NOTE_B4, NOTE_G4,
+  NOTE_D4, NOTE_E4, NOTE_B4,
+  NOTE_D5, NOTE_B4, NOTE_G4, NOTE_D4,
+  NOTE_E4, NOTE_B4, 0,
+  NOTE_E4, NOTE_B4, 0,
+  NOTE_D5, NOTE_F5, NOTE_D5, NOTE_B4,
+  NOTE_A4, NOTE_B4, NOTE_G4, NOTE_E4,
+  NOTE_E5, NOTE_B4, NOTE_D5, NOTE_F5,
+  NOTE_D5, NOTE_B4, NOTE_G4, NOTE_E4,
+  NOTE_E5, NOTE_B4, NOTE_D5, NOTE_F5,
+  NOTE_D5, NOTE_B4, NOTE_G4, NOTE_E4,
+  NOTE_A4, NOTE_B4, NOTE_G4, NOTE_E4,
+  NOTE_G5, NOTE_E5, NOTE_C5, NOTE_A4,
+  NOTE_E5, NOTE_C5, NOTE_B4, NOTE_G4,
+  NOTE_D5, NOTE_C5, NOTE_A4, NOTE_B4,
+  0
+}; // zeros are used to initalize the specific frequencies which were definied above.
+
+//Used MIDI sequencing to transpose the correct musical notes.
+int lucidDreamsMelody[] = {
+  4, 4, 4, 4,
+  4, 4, 2,
+  4, 4, 4, 4,
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 4, 4,
+  4, 4, 2,
+  4, 4, 4, 4,
+  4, 4, 2,
+  4, 4, 2,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  4, 4, 4, 4,
+  2
+};
 
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(melodyPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    playPiano(); //This code will run contuniously in order to fully initalize the specific notes when the buttons are pressed.
 }
+
+
+
+
+
+
+
+
+
+
+
