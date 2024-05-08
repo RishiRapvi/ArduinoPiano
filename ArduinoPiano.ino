ada115db75020ded9f2f94f43d45ace033941590
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2024-05-08 11:00:32 -0500

    made the piano and melody functions. code should work...

--- ArduinoPiano.ino
+++ ArduinoPiano.ino
@@ -59,6 +59,7 @@ int lucidDreamsMelody[] = {
 
 
 void setup() {
+
   Serial.begin(9600);
   pinMode(melodyPin, OUTPUT);
 }
@@ -68,13 +69,57 @@ void loop() {
     playPiano(); //This code will run contuniously in order to fully initalize the specific notes when the buttons are pressed.
 }
 
+void melodyDurationLogic(int m){
 
+  int size = sizeof(melody)/sizeof(int);
 
+  // Loop through the melody array and play each note with the corresponding tempo
+  for(int thisNote = 0; thisNote < size; thisNote++){
 
+    int noteDuration = 1000 / lucidDreamsMelody[thisNote];
+    tone(melodyPin, melody[thisNote], noteDuration);
+    int pauseBetweenNotes = noteDuration * 1.3;
+    delay(pauseBewteenNotes);
+    noTone(melodyPin);
 
+  }
 
+  if(s == 1){
+    Serial.println(" Lucid Dreams ");
+  }
+  return;
 
+}
 
+void playPiano(){
 
+  int buttons[] = {4, 5, 6, 7, 8, 9, 10, 11};
+  const char notes[] = {"DO", "RE", "MI", "FA", "SOL", "LA", "TI", "Lucid Dreams"};
+  int numButtons = sizeof(buttons)/ sizeof(int);
+  int melodySize = sizeof(melody)/ sizeof(int);
 
+  // Loop through each button pin
+  // Check if the button is pressed
+  // If pressed, play the corresponding note
 
+  for(i = 0; i < numButtons; i++){
+
+    if(digitalRead(buttons[i]) == HIGH){
+      Serial.print.ln(notes[i]);
+
+      if(i = 7){
+        melodyDurationLogic(1);
+        delay(Delay);
+
+       } else{
+          tone(Speaker, melody[i % melodySize]);
+          delay(Delay);
+          noTone(Speaker);
+
+        }
+
+    }
+
+  }
+  
+}
