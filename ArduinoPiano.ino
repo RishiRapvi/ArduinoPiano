2502cf0cef51195b87727d649ee20b5944759c7f
RishiRapvi <90780199+RishiRapvi@users.noreply.github.com>
2024-05-14 10:32:12 -0500

    updated some compiling errors

--- ArduinoPiano.ino
+++ ArduinoPiano.ino
@@ -5,7 +5,7 @@
 
 //Synopisis: This code is intended to play the simple piano notes, as well as play a special theme if the last button is pressed
 
-int melodyArray[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4_ NOTE_C5}; //do, re, mi, fa, sol, la, ti
+int melodyArray[] = {NOTE_C4, NOTE_D4, NOTE_E4, NOTE_F4, NOTE_G4, NOTE_A4, NOTE_B4, NOTE_C5}; //do, re, mi, fa, sol, la, ti
 
 //thanks to the pitches library, these notes, along with the respected functions, are initalzied for us
 int melody[] = {
@@ -79,14 +79,11 @@ void melodyDurationLogic(int m){
     int noteDuration = 1000 / lucidDreamsMelody[thisNote];
     tone(melodyPin, melody[thisNote], noteDuration);
     int pauseBetweenNotes = noteDuration * 1.3;
-    delay(pauseBewteenNotes);
+    delay(pauseBetweenNotes);
     noTone(melodyPin);
 
   }
 
-  if(s == 1){
-    Serial.println(" Lucid Dreams ");
-  }
   return;
 
 }
@@ -97,7 +94,7 @@ void melodyDurationLogic(int m){
 void playPiano(){
 
   int buttons[] = {4, 5, 6, 7, 8, 9, 10, 11};
-  const char notes[] = {"DO", "RE", "MI", "FA", "SOL", "LA", "TI", "Lucid Dreams"};
+  const char notes[][12] = {"DO", "RE", "MI", "FA", "SOL", "LA", "TI", "Lucid Dreams"};
   int numButtons = sizeof(buttons)/ sizeof(int);
   int melodySize = sizeof(melody)/ sizeof(int);
 
@@ -105,10 +102,10 @@ void playPiano(){
   // Check if the button is pressed
   // If pressed, play the corresponding note
 
-  for(i = 0; i < numButtons; i++){
+  for(int i = 0; i < numButtons; i++){
 
     if(digitalRead(buttons[i]) == HIGH){
-      Serial.print.ln(notes[i]);
+      Serial.println(notes[i]);
 
       if(i = 7){
         melodyDurationLogic(1);
